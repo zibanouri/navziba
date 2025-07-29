b3effe377b409d95cd4b45e7b96f605feb0b4a75
Ziba Nouri <zibanouri13621115@gmail.com>
2025-07-29 19:31:14 +0330

    edit style navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <nav className="relative top-0 left-0 right-0 z-40 transition-all duration-500 shadow-sm bg-white/90 backdrop-blur-md">
             <div className="max-w-5xl mx-auto p-4 sm:px-6 lg:px-8 rounded-lg">
                 <div className="flex justify-between items-center py-5">
-                    <div className="text-2xl font-bold text-slate-900">
+                    <div className="text-2xl font-bold text-slate-800">
                         Ziba Nouri
                     </div>
                     <div className="hidden md:flex space-x-8">
