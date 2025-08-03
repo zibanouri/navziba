5abee576efec4afb83070e98a1850b758185ddcd
Ziba Nouri <zibanouri13621115@gmail.com>
2025-08-03 11:22:27 +0330

    change style in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return(
         <footer className="py-12 px-4 border-t border-gray-500/30 bg-gray-50/40 backdrop-blur-sm">
-            <div className="max-w-6 mx-auto">Footer</div>
+            <div className="max-w-6xl mx-auto">Footer</div>
         </footer>
     )
 }
