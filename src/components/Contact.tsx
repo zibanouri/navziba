bd3f1b5d8e2b90afded3530e1bd7fc256a25fb16
Ziba Nouri <zibanouri13621115@gmail.com>
2025-08-06 16:11:42 +0330

    add contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,15 @@
-const Contact = () =>{
-    return (
-        <section id="contact">
-            <h2>Contact Section</h2>
-        </section>
-    )
+const Contact = () => {
+  return (
+    <section id="contact" className="py-20 px-4">
+      <div className="max-w-6xl mx-auto">
+        <div className="text-left mb-16">
+          <h2 className="text-3xl md:text-5xl mb-6 font-bold text-gray-900">
+            Get In Touch
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
 };
-
-
 
 export default Contact;
