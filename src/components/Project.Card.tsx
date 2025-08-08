f4ba8e93e22f4612a13849f6db71b8b3fa03e688
Ziba Nouri <zibanouri13621115@gmail.com>
2025-08-08 16:49:42 +0330

    initialize card image

--- src/components/Project.Card.tsx
+++ src/components/Project.Card.tsx
@@ -2,7 +2,13 @@ const ProjectCard = () => {
     return (
         <div className="max-w-80 mx-auto">
             <div className="bg-white/5 backdrop-blur-sm rounded-2xl overflow-y-hidden border border-gray-400/20 hover:border-gray-400/80 transition-all duration-300 group flex flex-col h-full">
-                Project Card
+                <div className="relative overflow-hidden">
+                    <div className="w-full h-48 bg-cover bg-center relative">
+                        <img src="/portfolio/public/project-image/project1.webp" alt="zibaCard" />
+
+                    </div>
+
+                </div>
             </div>
         </div>
     )
